9f433bffaf25d3e5800e1ca12df03e076332980e
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-07-30 16:19:03 +0200

    Initial Commit

---
+++ FetchContactsList/FetchContactsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchContactsListApp.swift
+//  FetchContactsList
+//
+//  Created by Michele Manniello on 30/07/22.
+//
+
+import SwiftUI
+
+@main
+struct FetchContactsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
